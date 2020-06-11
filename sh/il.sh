a0ecc6564cfed63fde6a108e4b02def605f7a4ad
Calvin Rodo <calvin.rodo@cds-snc.ca>
2020-06-10 20:45:23 -0400

    Fixing log listing

--- sh/il.sh
+++ sh/il.sh
@@ -1,13 +1,18 @@
 #!/bin/sh
 
 
-usage() { printf "Usage:\nRecommendation: %s -r message\n or\nTimeline: %s -t message " "$0" "$0" 1>&2; exit 1; }
+usage() { printf "
+Usage:
+Recommendation: %s -r recommendation
+Timeline: %s -t event
+Action Item: %s -a action item\n" "$0" "$0" "$0" 1>&2; exit 1; }
 
-folder="$HOME/incident-logs"
-today=$(date -u '+%Y-%m-%d')
-outputFile="$folder/$today.log"
-logDate="$(date -u '+%Y-%m-%d:%H:%M:%S')"
-while getopts ":t:r:a:l:" o; do
+readonly folder="$HOME/incident-logs"
+readonly today=$(date -u '+%Y-%m-%d')
+readonly outputFile="$folder/$today.log"
+readonly logDate="$(date -u '+%Y-%m-%d:%H:%M:%S')"
+
+while getopts ":t:r:a:l" o; do
     case "${o}" in
         r)
             printf "%s|RECOMMENDATION|%s\n" "$logDate" "${OPTARG}"  >> "$outputFile"
@@ -18,8 +23,8 @@ while getopts ":t:r:a:l:" o; do
         a ) 
             printf "%s|ACTION ITEM|%s\n" "$logDate" "${OPTARG}"  >> "$outputFile"
             ;;
-        l )
-            echo "$HOME/incident/*.log"
+        l)
+           ls -l "${folder}"/*.log
             ;;
         * )
             usage
