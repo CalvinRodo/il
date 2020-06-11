2614b403eb52009875c953d44478a39a7323b93d
Calvin Rodo <calvin.rodo@cds-snc.ca>
2020-06-10 20:47:56 -0400

    matching google style guide

--- sh/il.sh
+++ sh/il.sh
@@ -5,26 +5,26 @@ usage() { printf "
 Usage:
 Recommendation: %s -r recommendation
 Timeline: %s -t event
-Action Item: %s -a action item\n" "$0" "$0" "$0" 1>&2; exit 1; }
+Action Item: %s -a action item\n" "${0}" "${0}" "${0}" 1>&2; exit 1; }
 
-readonly folder="$HOME/incident-logs"
+readonly folder="${HOME}/incident-logs"
 readonly today=$(date -u '+%Y-%m-%d')
-readonly outputFile="$folder/$today.log"
+readonly outputFile="${folder}/${today}.log"
 readonly logDate="$(date -u '+%Y-%m-%d:%H:%M:%S')"
 
 while getopts ":t:r:a:l" o; do
     case "${o}" in
         r)
-            printf "%s|RECOMMENDATION|%s\n" "$logDate" "${OPTARG}"  >> "$outputFile"
+            printf "%s|RECOMMENDATION|%s\n" "${logDate}" "${OPTARG}"  >> "${outputFile}"
             ;;
         t )
-            printf "%s|TIMELINE|%s\n" "$logDate" "${OPTARG}"  >> "$outputFile"
+            printf "%s|TIMELINE|%s\n" "${logDate}" "${OPTARG}"  >> "${outputFile}"
             ;;
         a ) 
-            printf "%s|ACTION ITEM|%s\n" "$logDate" "${OPTARG}"  >> "$outputFile"
+            printf "%s|ACTION ITEM|%s\n" "${logDate}" "${OPTARG}"  >> "${outputFile}"
             ;;
         l)
-           ls -l "${folder}"/*.log
+            ls -l "${folder}"/*.log
             ;;
         * )
             usage
